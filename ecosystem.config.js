1ff80a397f9ad85cdc92009ffa286c67eab536d8
jorge <matheuskersul777@gmail.com>
2025-11-27 21:06:09 -0300

    Update ecosystem.config.js with cwd and log paths
    Added current working directory and updated log file paths to absolute paths.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   apps: [{
     name: 'meu-backend',
-    script: './src/index.js',
+    cwd: '/opt/apps/backend/current', // <--- ADICIONE ISSO AQUI (Mude se seu caminho for outro)
+    script: './src/index.js',         // Agora o ponto (.) vai funcionar
     instances: 1,
     autorestart: true,
     watch: false,
@@ -11,11 +12,11 @@ module.exports = {
       PORT: 3001,
       SECRET_NAME: 'money2-backend-dev-secret-rds',
       AWS_REGION: 'us-east-1',
-      // JWT_SECRET será injetado via variável de ambiente no application_start.sh
       JWT_SECRET: process.env.JWT_SECRET || ''
     },
-    error_file: './logs/pm2-error.log',
-    out_file: './logs/pm2-out.log',
+    // DICA: Use caminhos absolutos para logs também, para não perder eles
+    error_file: '/opt/apps/backend/current/logs/pm2-error.log',
+    out_file: '/opt/apps/backend/current/logs/pm2-out.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
     time: true
   }]
